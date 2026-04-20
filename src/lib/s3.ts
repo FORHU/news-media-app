d139f7c0349e82eaf641210a068d990ab86f41b3
bryanvaw <bryanvaw@gmail.com>
2026-04-20 15:42:42 +0800

    Fix: S3 env

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -3,8 +3,8 @@ import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 const s3Client = new S3Client({
     region: process.env.AWS_REGION!,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY!,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        accessKeyId: process.env.APP_AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.APP_AWS_SECRET_ACCESS_KEY!,
     },
 });
 
