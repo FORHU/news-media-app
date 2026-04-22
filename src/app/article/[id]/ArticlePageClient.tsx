fbff1ae9b76adc3189e2fb78dac77b285e2044c3
bryanvaw <bryanvaw@gmail.com>
2026-04-22 11:15:39 +0800

    Ads position fix

--- src/app/article/[id]/ArticlePageClient.tsx
+++ src/app/article/[id]/ArticlePageClient.tsx
@@ -114,7 +114,7 @@ export default function ArticlePageClient({
       }
     >
       <Header onOpenNewsletter={() => setIsNewsletterOpen(true)} />
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-0">
         <button
           type="button"
           onClick={() =>
